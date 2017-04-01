66035ed5224e91059b83b2ac3771fc98105e10b0
Brian Patino <patinobrian@gmail.com>
2017-04-01 12:03:57 -0700

    Close to finish exercise 1
    * add second socket
    * bind any available ip
    * bind any available port
    * listen on choosen port
    * send first request to mySocket server
    * accept response to psock(?)
    * add placeholder for verifying response random number

--- student.c
+++ student.c
@@ -1,4 +1,5 @@
 #include <stdlib.h> // fixes built-in error warnings
+#include <unistd.h>
 #include <stdio.h>  /* for NULL */
 #include <string.h>  /* for  strtok() */
 #include <ctype.h>  /* for atoi() */
@@ -17,10 +18,10 @@
 
 /* this is in netinet/in.h; included here for reference only.
 struct sockaddr_in {
-	short	sin_family;
-	u_short	sin_port;
-	struct	in_addr sin_addr;
-	char	sin_zero[8];
+  short sin_family;
+  u_short sin_port;
+  struct  in_addr sin_addr;
+  char  sin_zero[8];
 };
 */
 
@@ -39,6 +40,7 @@ static char *RCSId = "$Id: c-client.c,v 1.1 1997/01/19 20:34:49 calvert Exp $";
 #define SVR_PORT  3300
 #define SVR_ALTPORT  3301
 /* Port # of lab 3 server */
+#define LCL_ADDR "localhost"
 
 /***************************************************************************/
 /***************************************************************************/
@@ -147,28 +149,28 @@ int SockaddrToString(char *string, struct sockaddr_in *ss) {
  * Obtain the value after the specified token from string
  */
 char* nextTokenFromString(char* words, char* token) {
-	char* foundWord; // pointer to store the found word
-	char* word = strtok(words, " "); // get the first word before the specified split
-	int isNextToken = 0; // true if the specified token is found
-	while (word != NULL) { // keep looping until all tokens after split have been processed
-		if (isNextToken == 1) { // if the token was found before the current iteration
-			foundWord = word; // the next token after the specified token is obtained
-			isNextToken = 0; // set the counter to zero
-			break; // break out earlier for faster processing
-		}
-		if (strcmp(token, word) == 0) isNextToken = 1; // set the flag to true if the current token matches specified token
-		word = strtok(NULL, " "); // continue to the next token
-	}
-	return foundWord; // return either NULL or the found token
+  char* foundWord; // pointer to store the found word
+  char* word = strtok(words, " "); // get the first word before the specified split
+  int isNextToken = 0; // true if the specified token is found
+  while (word != NULL) { // keep looping until all tokens after split have been processed
+    if (isNextToken == 1) { // if the token was found before the current iteration
+      foundWord = word; // the next token after the specified token is obtained
+      isNextToken = 0; // set the counter to zero
+      break; // break out earlier for faster processing
+    }
+    if (strcmp(token, word) == 0) isNextToken = 1; // set the flag to true if the current token matches specified token
+    word = strtok(NULL, " "); // continue to the next token
+  }
+  return foundWord; // return either NULL or the found token
 }
 
 int main(int argc, char **argv) {
 
-  int mySocket, psock;
-  struct sockaddr_in destAddr, myAddr;
+  int mySocket, psock, newsock;
+  struct sockaddr_in destAddr, myAddr, freeAddr;
   int lineSize, myPort;
-  int sizeofmyAddr, sizeofdestAddr;
-  char inbuf[BUFSIZE], msgbuf[BUFSIZE], addrbuf[BUFSIZE], saddrbuf[BUFSIZE];
+  int sizeofmyAddr, sizeofdestAddr, sizeoffreeAddr;
+  char inbuf[BUFSIZE], msgbuf[BUFSIZE], addrbuf[BUFSIZE], saddrbuf[BUFSIZE], freeAddrbuf[BUFSIZE];
   char servernumBuf[BUFSIZE]; // initial server nubmer response
   char recServernum[BUFSIZE]; // after ack response servernum+1
 
@@ -179,6 +181,7 @@ int main(int argc, char **argv) {
    * to the server.  Set up the destination address information.
    ************************************************************/
 
+  // i
   if ((mySocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) die("couldn't allocate socket"); // exercise 0 socket
   if ((psock = socket(AF_INET, SOCK_STREAM, 0)) < 0) die ("couldn't allocate socket"); // exercise 1 socket
 
@@ -192,117 +195,152 @@ int main(int argc, char **argv) {
 
   // iii
   if (connect(mySocket, (struct sockaddr *) &destAddr, sizeof(destAddr)) < 0) die("failed to connect to server");
-
   printf("connected to server at %s\n", getTime());
 
   // iv
   sizeofmyAddr = sizeof(myAddr);
   if (getsockname(mySocket, (struct sockaddr *) &myAddr, &sizeofmyAddr) < 0) {
-    printf("getsockname failed on mySocket!\n");
+    printf("error: getsockname failed on mySocket!\n");
     addrbuf[0] = (char) 0;
   } else {
-    /* set up addrbuf */
     SockaddrToString(addrbuf, &myAddr);
   }
 
   sizeofdestAddr = sizeof(destAddr);
   if (getpeername(mySocket, (struct sockaddr *) &destAddr, &sizeofdestAddr) < 0) {
-    printf("getpeername failed on mySocket!\n");
+    printf("error: getpeername failed on mySocket!\n");
     saddrbuf[0] = (char) 0;
   } else {
     SockaddrToString(saddrbuf, &destAddr);
   }
 
-  // Generate request string
-  sprintf(msgbuf, "ex0 %s %s %d %s\n", saddrbuf, addrbuf, USERNUM, IDNAME);
+  /*
+   * Bind free port 
+   */
 
-  /**********************************************************************
-  * Exercise 1 make the connection
-  ***********************************************************************/
-  sprintf(bindAddrbuf, "%s-%d", SVR_ADDR, INADDR_ANY);
-  StringToSockaddr(bindAddrbuf, &bindMyAddr)
+  /* Set up free port */
+  freeAddr.sin_family = AF_INET;
+  freeAddr.sin_addr.s_addr = INADDR_ANY;
+  freeAddr.sin_port = htons(0);
+
+  // Bind the free port number
+  if (bind(psock, (struct sockaddr*) &freeAddr, sizeof(freeAddr)) < 0) {
+    close(psock);
+    close(mySocket);
+    printf("error: bind failed\n");
+    exit(1);
+  } 
 
   // Get binded sock address
-  if (getsockname(psock, (struct sockaddr *) &bindMyAddr, &sizeofmyAddr) < 0) {
-  	printf("getsockname failed on psock\n");
-  	bindAddrbuf[0] = (char) 0;
+  sizeoffreeAddr = sizeof(freeAddr);
+  if (getsockname(psock, (struct sockaddr *) &freeAddr, &sizeoffreeAddr) < 0) {
+    printf("getsockname failed on psock\n");
+    freeAddrbuf[0] = (char) 0;
   } else {
-  	SockaddrToString(bindAddrbuf, &bindMyAddr);
-  	printf("Binded port: %s\n", bindAddrbuf);
+    SockaddrToString(freeAddrbuf, &freeAddr);
+    printf("Binded port: %s\n", freeAddrbuf);
   }
 
-/*
- * your code goes here -- hint, use "send"
- */
-/*----------------- insert code ---------------------------*/
+  // call listen
+  if (listen(psock, 5) < 0) {
+    close(psock);
+    close(mySocket);
+    die("error: failed to listen to psock");
+  }
+
+  // Generate request string (different addrbuf socket for exercise 1)
+  sprintf(msgbuf, "ex1 %s %s %d %s\n", saddrbuf, freeAddrbuf, USERNUM, IDNAME);
+
 /*
  * Get the reply, first the greeting, then the random number.
  */
   int sendResult = send(mySocket, msgbuf, sizeof(msgbuf), 0);
-  if (sendResult < 0) die("failed to send to socket");
+  if (sendResult < 0) {
+    close(mySocket);
+    close(psock);
+    die("error: failed to send to socket");
+  }
 
-/* get the greeting  -- hint -- use "recv"
- */
-/*----------------- insert code ---------------------------*/
+  /* get the greeting  -- hint -- use "recv" */
 
   int receiveResult = recv(mySocket, inbuf, sizeof(inbuf), 0);
-  if (receiveResult < 0) die("failed to receive from socket");
+  if (receiveResult < 0) {
+    close(psock);
+    close(mySocket);
+    die("error: failed to receive from socket");
+  }
   inbuf[receiveResult] = '\0'; // terminate string
 
   // Check if OK exists
   if (strstr(inbuf, "OK") == NULL) {
-  	printf("errror: Not OK\n%s\n", inbuf);
-  	die("");
+    close(psock);
+    close(mySocket);
+    printf("errror: Not OK\n%s\n", inbuf);
+    exit(1);
   }
 
-  // Check usernum+1
+  /*// Check usernum+1
   char recUsernum[BUFSIZE];
   sprintf(recUsernum, "%d", (USERNUM + 1));
   if (strstr(inbuf, recUsernum) == NULL) {
-  	printf("error: Missing usernum+1\n%s\n", inbuf);
-  	die("");
+    printf("error: Missing usernum+1\n%s\n", inbuf);
+    die("");
+  }
+*/
+  // display the server response on connection
+  printf("\n---- Server response on connection ----\n%s\n", inbuf);
+
+  /* dont forget to get the random number */
+
+  // split string by white space
+  char* obtained = nextTokenFromString(inbuf, IDNAME);
+
+  // set servernum (random number)
+  strcpy(servernumBuf, obtained);
+  printf("Random Number: %s\n", servernumBuf);
+
+  /* parse out the servernum from the reply */
+  // Convert servernum to int
+  int servernum = strtol(servernumBuf, (char **)NULL, 10);
+  
+  // call accept()
+  newsock = accept(psock, (struct sockaddr *) &freeAddr, &sizeoffreeAddr);
+  if (newsock < 0) {
+    close(psock);
+    close(mySocket);
+    printf("error: accept failed\n");
+    exit(1);
   }
 
-  // display the server response on connection
-  printf("%s\n", inbuf);
+  // call recv on newsock to get new random number from server or until it returns zero
+  int psockRecv = recv(newsock, inbuf, sizeof(inbuf), 0);
+  if (psockRecv < 0) {
+    close(psock);
+    close(mySocket);
+    printf("error: receiving packets\n");
+    exit(1);
+  }
+  inbuf[psockRecv] = '\0'; // terminal string
 
-	/* dont forget to get the random number */
-	/*----------------- insert code ---------------------------*/
-	// split string by white space
-	char* obtained = nextTokenFromString(inbuf, IDNAME);
+  // Verify random number has been received
 
-	// set servernum (random number)
-	strcpy(servernumBuf, obtained);
-	printf("Random Number: %s\n", servernumBuf);
-
-/* parse out the servernum from the reply */
-/*----------------- insert code ---------------------------*/
-
-	// Convert servernum to int
-	int servernum = strtol(servernumBuf, (char **)NULL, 10);
-
-/* construct and send the client ack string*/
-/*----------------- insert code ---------------------------*/
-
-	// Construct ACK
+  /* construct and send the client ack string*/
+  /*// Construct ACK
   sprintf(msgbuf, "ex0 %d %d \n", USERNUM+2, servernum+1);
 
   // Send ACK
   int ackResult = send(mySocket, msgbuf, sizeof(msgbuf), 0);
-  if (ackResult < 0) die("faield to send ACK to socket");
+  if (ackResult < 0) die("error: failed to send ACK to socket");*/
 
-/* wait for reply */
-/*----------------- insert code ---------------------------*/
-
-  // Check receive result
+  /*// Check receive result
   int ackReceiveResult = recv(mySocket, inbuf, sizeof(inbuf), 0);
-  if (ackReceiveResult < 0) die("failed to receive from socket");
+  if (ackReceiveResult < 0) die("error: failed to receive from socket");
   inbuf[ackReceiveResult] = '\0'; // terminate string
 
   // Verify that OK was received
   if (strstr(inbuf, "OK") == NULL) {
-  	printf("error: Not OK\n%s\n", inbuf);
-  	die("");
+    printf("error: Not OK\n%s\n", inbuf);
+    die("");
   }
 
   // display the received ACK response
@@ -311,12 +349,12 @@ int main(int argc, char **argv) {
   // Check servernum+1
   obtained = nextTokenFromString(inbuf, "OK");
   strcpy(recServernum, obtained);
-  printf("Received servernum+1: %s", recServernum);
+  printf("Received servernum+1: %s", recServernum);*/
 
-/* now close the connection -- hint -- use close() */
-/*----------------- insert code ---------------------------*/
+  /* now close the connection -- hint -- use close() */
 
   // all values needed should have been saved, close the connection
+  close(psock);
   close(mySocket);
   return 0;
 }
